ade216b635799eafe08327b84b136141ef9daabf
sunzeyong <sunzeyong@virtaitech.com>
2024-08-13 21:18:30 +0800

    feat: add goconvey

--- utdemo/biz.go
+++ utdemo/biz.go
@@ -29,3 +29,39 @@ func partition(input []int, l, r int) int {
 	input[i], input[r] = input[r], input[i]
 	return i
 }
+
+func HeapSort(input []int) {
+	// build heap
+	buildHeap(input)
+
+	// sort
+	for i := len(input) - 1; i > 0; i-- {
+		input[i], input[0] = input[0], input[i]
+		heapify(input, i-1, 0)
+	}
+}
+
+func buildHeap(input []int) {
+	for i := (len(input) - 1) / 2; i >= 0; i-- {
+		heapify(input, len(input)-1, i)
+	}
+}
+
+// input[0:n]中index=i进行向下堆化
+func heapify(input []int, n, i int) {
+	for {
+		left, right := 2*i+1, 2*i+2
+		maxPos := i
+		if left <= n && input[left] > input[maxPos] {
+			maxPos = left
+		}
+		if right <= n && input[right] > input[maxPos] {
+			maxPos = right
+		}
+		if i == maxPos {
+			return
+		}
+		input[maxPos], input[i] = input[i], input[maxPos]
+		i = maxPos
+	}
+}
--- utdemo/biz_test.go
+++ utdemo/biz_test.go
@@ -3,9 +3,11 @@ package utdemo
 import (
 	"testing"
 
+	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
 )
 
+// 使用testify库
 func TestQuickSort(t *testing.T) {
 	// 表驱动ut
 	cases := []struct {
@@ -14,8 +16,8 @@ func TestQuickSort(t *testing.T) {
 		expected []int
 	}{
 		{"case1", []int{7, 6, 5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5, 6, 7}},
-		{"case1", []int{1, 6, 3, 1, 5, 4, 1}, []int{1, 1, 1, 3, 4, 5, 6}},
-		{"case1", []int{3, 4, 0, -1, -1, 5}, []int{-1, -1, 0, 3, 4, 5}},
+		{"case2", []int{1, 6, 3, 1, 5, 4, 1}, []int{1, 1, 1, 3, 4, 5, 6}},
+		{"case3", []int{3, 4, 0, -1, -1, 5}, []int{-1, -1, 0, 3, 4, 5}},
 	}
 
 	for _, c := range cases {
@@ -25,4 +27,26 @@ func TestQuickSort(t *testing.T) {
 			assert.Equal(t, c.expected, c.input)
 		})
 	}
+}
+
+// 使用goconvey, 提供了更好的结果展示和UI
+func TestHeapSort(t *testing.T) {
+	Convey("基础测试", t, func() {
+		tt := []struct {
+			name     string
+			input    []int
+			expected []int
+		}{
+			{"case1", []int{7, 6, 5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5, 6, 7}},
+			{"case2", []int{1, 6, 3, 1, 5, 4, 1}, []int{1, 1, 1, 3, 4, 5, 6}},
+			{"case3", []int{3, 4, 0, -1, -1, 5}, []int{-1, -1, 0, 3, 4, 5}},
+		}
+
+		for _, tc := range tt {
+			Convey(tc.name, func() {
+				HeapSort(tc.input)
+				So(tc.input, ShouldResemble, tc.expected)
+			})
+		}
+	})
 }
